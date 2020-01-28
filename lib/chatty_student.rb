938607b2160408ff25680c71b8ad107d6abfad82
Pledge Davis <pledgedavis14@gmail.com>
2020-01-28 22:45:32 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,10 +5,14 @@ class ChattyStudent < Student
       puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
-  def raise_hand(message)
-      super(puts "Pick me!")
-      # puts "Pick me!"
+  def raise_hand
 
+      # 10.times { super  }
+      # num1.times { |i| print i, " " }
+
+      10.times do
+        super
+      end
   end
 
 
