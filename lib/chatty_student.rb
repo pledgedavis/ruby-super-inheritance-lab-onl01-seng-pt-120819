c6cd4097985602568db933467344a26048d9f5d4
Pledge Davis <pledgedavis14@gmail.com>
2020-01-28 22:39:39 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,7 +7,7 @@ class ChattyStudent < Student
 
   def raise_hand
       super puts "Pick me!"
-      10.times { super }
+      10.times { super print super, "Pick me!" }
       # num1.times { |i| print i, " " }
       # puts "Pick me!"
 
